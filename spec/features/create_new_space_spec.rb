4d58f40d11cc3cafaf7b0c56780dedd78e6d18b8
gerauf <fo4786@gmail.com>
2016-05-16 17:07:46 +0100

    extended feature test

--- spec/features/create_new_space_spec.rb
+++ spec/features/create_new_space_spec.rb
@@ -2,6 +2,8 @@ feature "create spaces" do
   scenario "create new space" do
     visit '/spaces/new'
     fill_in :name, with: "Commercial Road"
+    fill_in :description, with: "Tidy little place"
+    fill_in :value, with: 100
     expect{click_button("create space")}.to change(Space, :count).by 1
     expect(current_path).to eq '/'
     within "ul#spaces" do
