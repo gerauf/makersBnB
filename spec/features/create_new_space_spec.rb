430bd2945c80b48dce45e298ca3b8edc469269f8
gerauf <fo4786@gmail.com>
2016-05-16 17:52:19 +0100

    added style sheet

--- spec/features/create_new_space_spec.rb
+++ spec/features/create_new_space_spec.rb
@@ -3,11 +3,14 @@ feature "create spaces" do
     visit '/spaces/new'
     fill_in :name, with: "Commercial Road"
     fill_in :description, with: "Tidy little place"
-    fill_in :value, with: 100
+    fill_in :price, with: 100
     expect{click_button("create space")}.to change(Space, :count).by 1
     expect(current_path).to eq '/'
     within "ul#spaces" do
       expect(page).to have_content "Commercial Road"
+      expect(page).to have_content "Tidy little place"
+      expect(page).to have_content "Cost per night £100"
     end
   end
+
 end
