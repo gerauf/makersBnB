f0ab395e5955ff28e11c3e4ccb0affcb68aa52a9
gerauf <fo4786@gmail.com>
2016-05-16 13:44:05 +0100

    initial db config set up

--- app/makersBnB.rb
+++ app/makersBnB.rb
@@ -1,9 +1,14 @@
+ENV["RACK_ENV"] ||= "development"
+
 require 'sinatra/base'
+
+require_relative 'data_mapper_setup'
+
+require_relative 'server'
 
 class MakersBnB < Sinatra::Base
   get '/' do
     'Hello MakersBnB!'
-    
   end
 
   # start the server if ruby file executed directly
