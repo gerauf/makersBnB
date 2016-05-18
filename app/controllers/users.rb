08a4c03af8c2779a697cc7cf32ef7d82d48fd012
gerauf <fo4786@gmail.com>
2016-05-18 09:12:43 +0100

    wednesday morning

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -13,10 +13,10 @@ class MakersBnB < Sinatra::Base
 
   post '/users' do
     @user = User.create(first_name: params[:first_name],
-            last_name: params[:last_name],
-            email: params[:email],
-            password: params[:password],
-            password_confirmation: params[:password_confirm])
+                        last_name: params[:last_name],
+                        email: params[:email],
+                        password: params[:password],
+                        password_confirmation: params[:password_confirm])
     session[:user_id] = @user.id
     if @user.save
       redirect '/'
