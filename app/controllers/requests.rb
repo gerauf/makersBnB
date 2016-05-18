bfa887ba86b58763a3888cdd17a512478282dd3d
pixelandpage <mags@pixelandpage.co.uk>
2016-05-18 12:40:14 +0100

    updated workflow for making requests

--- app/controllers/requests.rb
+++ app/controllers/requests.rb
@@ -3,7 +3,8 @@ class MakersBnB < Sinatra::Base
   post '/requests' do
     space_id = params[:space_id]
     user_id = current_user.id
-    Request.create(user_id: user_id, space_id: space_id)
+    Request.first_or_create(user_id: user_id,
+                            space_id: space_id) unless own_space? space_id
     redirect '/'
   end
 
