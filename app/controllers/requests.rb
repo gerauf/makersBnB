3a1bfb3b62ba1a5b1ea0dd0fa7bc709a9ecc8a9e
gerauf <fo4786@gmail.com>
2016-05-18 11:38:20 +0100

    display requests made and recieved

--- app/controllers/requests.rb
+++ app/controllers/requests.rb
@@ -8,6 +8,8 @@ class MakersBnB < Sinatra::Base
   end
 
   get '/requests' do
+    @requests_made = Request.all(user_id: current_user.id)
+    @requests_received = Request.all(space_id: Space.first(user_id: current_user.id).id)
     erb :'requests/index'
   end
 
